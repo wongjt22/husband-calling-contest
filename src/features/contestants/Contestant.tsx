3afbc161666386ee8822c31d559bb88786b8f3c5
Jackson Wong <wongjt@berkeley.edu>
2023-09-05 09:27:18 -0700

    contestants

---
+++ src/features/contestants/Contestant.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Button } from '@/components/Elements/Button';
+import './Contestant.css';
+
+type ContestantProps = {
+  name: string;
+  imageUrl: string;
+};
+
+export const Contestant: React.FC<ContestantProps> = ({ name, imageUrl }) => {
+  const [voteCount, setVoteCount] = useState<number>(0);
+
+  const handleVoteClick = () => {
+    setVoteCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div className="image-card">
+      <div className="image-card-name">{name}</div>
+      <img src={imageUrl} alt={name} className="image-card-image" />
+      <div className="image-card-footer">
+        <div className="vote-count">Votes: {voteCount}</div>
+        <div className="vote-button">
+          <Button text="Vote" onClick={handleVoteClick} />
+        </div>
+        
+      </div>
+    </div>
+  );
+};
--- src/features/contestants/Contestants.tsx
+++ src/features/contestants/Contestants.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { MainLayout } from '@/components/Layout'
+import { Contestant } from './Contestant'
+import './Contestants.css'
 
 export const Contestants = () => {
+
+  const contestants = [
+    {
+      name: "Joe",
+      imageUrl: "https://t3.ftcdn.net/jpg/02/22/10/62/360_F_222106228_NP5f0gXi3JOCgmaTsEyg7RuyKgwDLGuY.jpg"
+    },
+    {
+      name: "Joe",
+      imageUrl: "https://t3.ftcdn.net/jpg/02/22/10/62/360_F_222106228_NP5f0gXi3JOCgmaTsEyg7RuyKgwDLGuY.jpg"
+    },
+    {
+      name: "Joe",
+      imageUrl: "https://t3.ftcdn.net/jpg/02/22/10/62/360_F_222106228_NP5f0gXi3JOCgmaTsEyg7RuyKgwDLGuY.jpg"
+    },
+    {
+      name: "Joe",
+      imageUrl: "https://t3.ftcdn.net/jpg/02/22/10/62/360_F_222106228_NP5f0gXi3JOCgmaTsEyg7RuyKgwDLGuY.jpg"
+    }
+  ]
   return (
-    <div>Contestants</div>
+    <MainLayout>
+      <h1>Contestants</h1>
+      <div className='contestants'>
+        {contestants.map(contestant => (
+          <Contestant name={contestant.name} imageUrl={contestant.imageUrl}/>
+        ))}
+      </div>
+    </MainLayout>
   )
 }
 
